f0d4fef4d1957b3875d82e38a9a91d02fa1abcf4
Victor Allen <vamuigua@gmail.com>
2025-01-09 21:08:05 +0300

    update: add route page components

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('#app');
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app');
