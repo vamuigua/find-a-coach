07c3808915046644a5c2e4030a0b9d45519e999a
Victor Allen <vamuigua@gmail.com>
2025-02-16 11:29:18 +0300

    update: add coaches to vuex

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -2,7 +2,7 @@
   <section>
     <base-card>
       <h2>Register as a coach now!</h2>
-      <CoachForm />
+      <CoachForm @save-data="saveData" />
     </base-card>
   </section>
 </template>
@@ -14,6 +14,12 @@ export default {
   name: 'CoachRegistration',
   components: {
     CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    },
   },
 };
 </script>
